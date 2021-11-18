b5024fbc0558bb28e84897801c8450be32d05652
Sérgio Raphael A. Sampaio <67436514+Svetrar@users.noreply.github.com>
2021-11-18 06:08:12 -0300

    Update av1_excluirPergunta.php

--- av1_excluirPergunta.php
+++ av1_excluirPergunta.php
@@ -1,3 +1,49 @@
+<?php
+	
+	$ID = "";
+	
+	$linhas = array();
+	
+	if ($_SERVER["REQUEST_METHOD"] == "GET") 
+	{
+		$ID = $_GET["id"];
+		
+		$arquivoAluno = fopen("perguntasNovas.txt", "r") or die("Erro na abertura do arquivo");
+		
+		$header =  fgets($arquivoAluno);
+		
+		$x = 0;
+		
+		$colunasArquivo = array();
+		$achei = false;
+		
+		while (!feof($arquivoAluno)) 
+		{
+			$linhas[] = fgets($arquivoAluno);
+			
+			echo $linhas[$x];
+			
+			$colunasArquivo = explode(";", $linhas[$x]);
+			
+			echo "Arquivo: $colunasArquivo[0] / ID PERGUNTA:  $ID<br>";	
+			
+			
+			if ($colunasArquivo[0] == $ID) 
+			{
+				echo "ACHEI";
+				$achei = true;
+				break;
+			}			
+			$x++;
+			
+		}
+		if (!$achei) 
+		{
+			echo "ID: $ID não encontrado";
+		}
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -6,7 +52,7 @@
 
 <body>
 
-<h1>3DAW - Excluir Perguntas</h1>
+<h1>3DAW - Buscar p/ Alterar Perguntas</h1>
 
 <br>
 	<a href="av1_home.php">Home</a><br>
@@ -15,6 +61,25 @@
 	<a href="av1_listarPergunta.php">Listar Todas as Perguntas</a><br>
 	<a href="av1_listarUmaPergunta.php">Listar Uma Pergunta</a><br>
 	<a href="av1_excluirUmaPergunta.php">Excluir</a><br>
+
+<br>
+
+<br>
+
+<form action="av1_excluirUmaPergunta.php" method="POST">
+
+        Identificação da Pergunta: <input type="text" name="id" value="<?php echo $colunasArquivo[0]; ?>"> <br>
+			
+		Pergunta: <input type="text" name="p" value="<?php echo $colunasArquivo[1]; ?>"> <br>
+		
+		Qtd de pontos: <input type=text name="q" value="<?php echo $colunasArquivo[2]; ?>"> <br>
+	
+		Grau de dificuldade: <input type=text name="d" value="<?php echo $colunasArquivo[3]; ?>"> <br>
+			 	
+        <br><br>
+			
+        <input type="submit" value="Busca Excluir">
+</form>
 
 <br>
 
